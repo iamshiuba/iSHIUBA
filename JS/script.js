f3b8b8041b54528deef5544af7268343dd4a4589
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-08-31 11:02:28 -0300

    stuff

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Nav from './nav';
+
+ReactDOM.render(<Nav />, document.getElementById('nav'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  entry: './JS/script.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
