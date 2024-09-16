b4cbfd6e23a6f354d5ec7b78f4ba210b979da8f6
iamshiuba <ian.pazeamor@outlook.com>
2024-09-15 22:40:39 -0300

    again upt 0.2.0

--- javascript/nav.js
+++ javascript/nav.js
@@ -11,3 +11,24 @@ function importNav() {
 
 // Chamar a função para importar o nav
 importNav();
+
+function setLanguage(language) {
+  const elementsToTranslate = document.querySelectorAll("[data-translate]");
+  elementsToTranslate.forEach((element) => {
+    const translationKey = element.getAttribute("data-translate");
+    element.innerHTML = translations[language][translationKey];
+  });
+
+  // Atualizar textos fora do "data-translate"
+  document.querySelector(".greetings h1").innerHTML =
+    translations[language].greeting;
+  document.querySelector(".greetings p").innerHTML =
+    translations[language].mainMessage;
+  document.querySelector(
+    "footer p"
+  ).innerHTML = `&copy; 2023 - 2024 IamSHIUBA. ${translations[language].footer}`;
+
+// Alterar o texto do botão do seletor de idioma
+const languageDropdownButton = document.getElementById("languageDropdown");
+  languageDropdownButton.innerHTML = language === "en" ? "EN" : language === "br" ? "BR" : "JP";
+}
