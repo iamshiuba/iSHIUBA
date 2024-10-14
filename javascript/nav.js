2e2f61abc41b385c50ad3b1669f42a74b523e2b9
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-10-14 13:56:27 -0300

    Squashed commit of the following:
    commit 6b8657cd929fc44ab9bdf99e45f76a3865b4b21d
    Author: iamshiuba <ian.pazeamor@outlook.com>
    Date:   Tue Sep 17 01:27:39 2024 -0300
    
        Update 0.3.0
    
    commit d22abe93735f05309ef8f5ea27a3c46672ee9482
    Author: iamshiuba <ian.pazeamor@outlook.com>
    Date:   Mon Sep 16 17:22:35 2024 -0300
    
        Update 0.2.1
    
    commit 247a665fdd99c46df03c340f8da3284db3e4919a
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sun Sep 15 22:43:21 2024 -0300
    
        Create README.md
    
    commit 7b586fdb73ae39dccbaefd76da633a15f52e2433
    Merge: b4cbfd6 097c076
    Author: iamshiuba <ian.pazeamor@outlook.com>
    Date:   Sun Sep 15 22:40:42 2024 -0300
    
        Merge branch 'iSHIUBAa6_9' of https://github.com/iamshiuba/iSHIUBA into iSHIUBAa6_9
    
    commit b4cbfd6e23a6f354d5ec7b78f4ba210b979da8f6
    Author: iamshiuba <ian.pazeamor@outlook.com>
    Date:   Sun Sep 15 22:40:39 2024 -0300
    
        again upt 0.2.0
    
    commit 097c076352026b31837f0ab77c4fa7473312b815
    Author: iamshiuba <ian.pazeamor@outlook.com>
    Date:   Sun Sep 15 22:30:54 2024 -0300
    
        bruh fix
    
    commit 3186cb7471364391984e0f5e71e5e1a2c897a4e9
    Author: iamshiuba <ian.pazeamor@outlook.com>
    Date:   Sun Sep 15 22:27:43 2024 -0300
    
        fix
    
    commit 1159928117c2068af509664e0a8c68db2742cc10
    Author: iamshiuba <ian.pazeamor@outlook.com>
    Date:   Sun Sep 15 22:25:11 2024 -0300
    
        Update 0.2.0
    
    commit 9dd3a425420463e1dd1e52f677a9ab15bac9a7d1
    Author: ishiuba <iamshiuba@hotmail.com>
    Date:   Sat Sep 14 16:37:08 2024 -0300
    
        Update 0.1.3
    
    commit 7bde5de8eeb09b9551ad7409f34696bd4763fbd8
    Author: ishiuba <iamshiuba@hotmail.com>
    Date:   Wed Sep 11 00:39:03 2024 -0300
    
        Update 0.1.2
    
    commit a87d71d9f99db8b2ec9b2bdfeeb0f1ff8b7e2957
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sun Sep 8 21:11:07 2024 -0300
    
        fix
    
    commit c31e49fb3480e59aa5c759d1580f7bfc42a382fc
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 15:35:05 2024 -0300
    
        test2
    
    commit ad034cb09ea4b55f9a2ceef71d2d36193be2af21
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 15:25:52 2024 -0300
    
        test
    
    commit 5b1f985f7c7742d173c4f24343e277e292c643b4
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 15:22:02 2024 -0300
    
        Update README.md
    
    commit 689a87220ec39f87014a151d9ca9cef8fcf0e36d
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 15:20:27 2024 -0300
    
        Update README.md
    
    commit 59eae4ba46ccc88de039077df591d718a1116236
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 02:20:00 2024 -0300
    
        Update 0.1.1
    
    commit 61131f74eb8d7704b9e11c1f48af89d74855f131
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Fri Sep 6 18:29:14 2024 -0300
    
        Update index.html
    
    commit e6f6f22ba6d7861d54e6af4f9579007956518658
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Fri Sep 6 15:16:02 2024 -0300
    
        Update index.html
    
    commit 5daed75baf2a696f552dcdf8f738bcc4680f1a69
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Fri Sep 6 12:52:31 2024 -0300
    
        Update footer.html
    
    commit 26d8bc751b3503a888cedc1b44519df2ca92f45f
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Fri Sep 6 12:33:04 2024 -0300
    
        Update about.html

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,10 +1,10 @@
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
 
 // Função para importar o nav
 function importNav() {
-  const navHtml = fetch('nav.html')
-    .then(response => response.text())
-    .then(html => {
+  const navHtml = fetch("nav.html")
+    .then((response) => response.text())
+    .then((html) => {
       nav.innerHTML = html;
     });
 }
@@ -18,17 +18,25 @@ function setLanguage(language) {
     const translationKey = element.getAttribute("data-translate");
     element.innerHTML = translations[language][translationKey];
   });
-
   // Atualizar textos fora do "data-translate"
   document.querySelector(".greetings h1").innerHTML =
     translations[language].greeting;
   document.querySelector(".greetings p").innerHTML =
     translations[language].mainMessage;
-  document.querySelector(
-    "footer p"
-  ).innerHTML = `&copy; 2023 - 2024 IamSHIUBA. ${translations[language].footer}`;
-
-// Alterar o texto do botão do seletor de idioma
-const languageDropdownButton = document.getElementById("languageDropdown");
-  languageDropdownButton.innerHTML = language === "en" ? "EN" : language === "br" ? "BR" : "JP";
+  document.querySelector("footer p");
+  // Alterar o texto do botão do seletor de idioma
+  const languageDropdownButton = document.getElementById("languageDropdown");
+  languageDropdownButton.innerHTML =
+    language === "en" ? "EN" : language === "br" ? "BR" : "JP";
+  localStorage.setItem("selectedLanguage", language);
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Verificar se há um idioma selecionado em localStorage
+  const selectedLanguage = localStorage.getItem("selectedLanguage");
+
+  // Se houver um idioma selecionado, aplicá-lo
+  if (selectedLanguage) {
+    setLanguage(selectedLanguage);
+  }
+});
--- javascript/translations.js
+++ javascript/translations.js
@@ -1,31 +1,35 @@
 const translations = {
   en: {
+    ishiuba: "iSHIUBA",
     videos: "Videos",
     about: "About",
     greeting: "Hello, world!",
     mainMessage:
-      "This is the main page of iSHIUBA. <br/> The website is still under construction, but you can check out<br/> some of my <a href='videos.html'>videos</a>, my <a href='about.html'>about page</a> or my <a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>.",
+      "This is the main page of iSHIUBA. <br/> The website is still under construction, but you can check out<br/> some of my <a href='#youtube'>videos</a>, the <a href='#about'>about</a> section or my <a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>.",
     footer: "All rights reserved.",
     videosTitle: "Videos",
     videosDescription:
-      "This is the videos page.<br /> Here are some videos to watch:",
+      "This is the videos section,<br /> here are some videos to watch:",
     aboutTitle: "About",
     aboutMessage:
       "I made this website for fun.<br /> Hope you enjoy coz I'm about to make it better and better! <br />Thank you for visiting!",
+    footer: "All rights reserved.",
   },
   br: {
+    ishiuba: "iSHIUBA",
     videos: "Vídeos",
     about: "Sobre",
     greeting: "Olá, mundo!",
     mainMessage:
-      "Esta é a página principal do iSHIUBA. <br /> O site ainda está em construção, mas você pode conferir<br /> alguns dos meus <a href='videos.html'>vídeos</a>, minha <a href='about.html'>página sobre</a> ou meu <a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>.",
+      "Esta é a página principal de iSHIUBA. <br /> O site ainda está em construção, mas você pode conferir<br /> alguns dos meus <a href='#youtube'>vídeos</a>, a seção <a href='#about'>sobre</a> ou meu <a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>.",
     footer: "Todos os direitos reservados.",
     videosTitle: "Vídeos",
     videosDescription:
-      "Esta é a página de vídeos.<br /> Aqui estão alguns vídeos para assistir:",
+      "Esta é a seção de vídeos,<br /> aqui estão alguns vídeos para assistir:",
     aboutTitle: "Sobre",
     aboutMessage:
       "Eu fiz este site por diversão.<br/> Espero que você goste, pois estou prestes a melhorá-lo cada vez mais!<br/> Obrigado pela visita!",
+    footer: "Todos os direitos reservados.",
   },
   jp: {
     ishiuba: "アイシウバ",
@@ -33,13 +37,14 @@ const translations = {
     about: "約",
     greeting: "ハロー、ワールド！",
     mainMessage:
-      "これはアイシウバのメインページです。<br/> ウェブサイトはまだ建設中ですが、<br/> <a href='videos.html'>ビデオ</a>、<a href='about.html'>自己紹介ページ</a>、<br/>または<a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>をご覧いただけます。",
+      "これはアイシウバのメインページです。<br/> ウェブサイトはまだ建設中ですが、<br/> <a href='#youtube'>ビデオ</a>、<a href='#about'>自己紹介</a> セクション、<br/>または<a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>をご覧いただけます。",
     footer: "全著作権所有。",
     videosTitle: "ビデオ",
     videosDescription:
-      "これはビデオページです。<br /> こちらには視聴するビデオがいくつかあります：",
+      "これはビデオセクションです。<br /> 視聴すべきビデオをいくつか紹介します:",
     aboutTitle: "約",
     aboutMessage:
       "私はこのウェブサイトを楽しむために作りました。<br /> あなたが楽しんでくれることを願っています！ これからますます良くしていきます！<br /> 訪問していただきありがとうございます！",
+    footer: "全著作権所有。",
   },
 };
