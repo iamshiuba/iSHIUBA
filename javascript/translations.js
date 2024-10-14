99fd19869e9acca85b51daf0ccf7b6c361d58edf
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-10-14 13:54:40 -0300

    Squashed commit of the following:
    commit 1159928117c2068af509664e0a8c68db2742cc10
    Author: iamshiuba <ian.pazeamor@outlook.com>
    Date:   Sun Sep 15 22:25:11 2024 -0300
    
        Update 0.2.0
    
    commit 9dd3a425420463e1dd1e52f677a9ab15bac9a7d1
    Author: ishiuba <iamshiuba@hotmail.com>
    Date:   Sat Sep 14 16:37:08 2024 -0300
    
        Update 0.1.3
    
    commit 7bde5de8eeb09b9551ad7409f34696bd4763fbd8
    Author: ishiuba <iamshiuba@hotmail.com>
    Date:   Wed Sep 11 00:39:03 2024 -0300
    
        Update 0.1.2
    
    commit a87d71d9f99db8b2ec9b2bdfeeb0f1ff8b7e2957
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sun Sep 8 21:11:07 2024 -0300
    
        fix
    
    commit c31e49fb3480e59aa5c759d1580f7bfc42a382fc
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 15:35:05 2024 -0300
    
        test2
    
    commit ad034cb09ea4b55f9a2ceef71d2d36193be2af21
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 15:25:52 2024 -0300
    
        test
    
    commit 5b1f985f7c7742d173c4f24343e277e292c643b4
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 15:22:02 2024 -0300
    
        Update README.md
    
    commit 689a87220ec39f87014a151d9ca9cef8fcf0e36d
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 15:20:27 2024 -0300
    
        Update README.md
    
    commit 59eae4ba46ccc88de039077df591d718a1116236
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Sat Sep 7 02:20:00 2024 -0300
    
        Update 0.1.1
    
    commit 61131f74eb8d7704b9e11c1f48af89d74855f131
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Fri Sep 6 18:29:14 2024 -0300
    
        Update index.html
    
    commit e6f6f22ba6d7861d54e6af4f9579007956518658
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Fri Sep 6 15:16:02 2024 -0300
    
        Update index.html
    
    commit 5daed75baf2a696f552dcdf8f738bcc4680f1a69
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Fri Sep 6 12:52:31 2024 -0300
    
        Update footer.html
    
    commit 26d8bc751b3503a888cedc1b44519df2ca92f45f
    Author: iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
    Date:   Fri Sep 6 12:33:04 2024 -0300
    
        Update about.html

---
+++ javascript/translations.js
@@ -0,0 +1,45 @@
+const translations = {
+  en: {
+    videos: "Videos",
+    about: "About",
+    greeting: "Hello, world!",
+    mainMessage:
+      "This is the main page of iSHIUBA. <br/> The website is still under construction, but you can check out<br/> some of my <a href='videos.html'>videos</a>, my <a href='about.html'>about page</a> or my <a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>.",
+    footer: "All rights reserved.",
+    videosTitle: "Videos",
+    videosDescription:
+      "This is the videos page.<br /> Here are some videos to watch:",
+    aboutTitle: "About",
+    aboutMessage:
+      "I made this website for fun.<br /> Hope you enjoy coz I'm about to make it better and better! <br />Thank you for visiting!",
+  },
+  br: {
+    videos: "Vídeos",
+    about: "Sobre",
+    greeting: "Olá, mundo!",
+    mainMessage:
+      "Esta é a página principal do iSHIUBA. <br /> O site ainda está em construção, mas você pode conferir<br /> alguns dos meus <a href='videos.html'>vídeos</a>, minha <a href='about.html'>página sobre</a> ou meu <a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>.",
+    footer: "Todos os direitos reservados.",
+    videosTitle: "Vídeos",
+    videosDescription:
+      "Esta é a página de vídeos.<br /> Aqui estão alguns vídeos para assistir:",
+    aboutTitle: "Sobre",
+    aboutMessage:
+      "Eu fiz este site por diversão.<br/> Espero que você goste, pois estou prestes a melhorá-lo cada vez mais!<br/> Obrigado pela visita!",
+  },
+  jp: {
+    ishiuba: "アイシウバ",
+    videos: "ビデオ",
+    about: "約",
+    greeting: "ハロー、ワールド！",
+    mainMessage:
+      "これはアイシウバのメインページです。<br/> ウェブサイトはまだ建設中ですが、<br/> <a href='videos.html'>ビデオ</a>、<a href='about.html'>自己紹介ページ</a>、<br/>または<a href='https://github.com/iamshiuba' rel='noopener' target='_blank'>GitHub</a>をご覧いただけます。",
+    footer: "全著作権所有。",
+    videosTitle: "ビデオ",
+    videosDescription:
+      "これはビデオページです。<br /> こちらには視聴するビデオがいくつかあります：",
+    aboutTitle: "約",
+    aboutMessage:
+      "私はこのウェブサイトを楽しむために作りました。<br /> あなたが楽しんでくれることを願っています！ これからますます良くしていきます！<br /> 訪問していただきありがとうございます！",
+  },
+};
