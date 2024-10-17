eceb809ec517c5062dbfd5ab8796b95047bf364d
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-10-17 00:35:23 -0300

    update b5.2.1

--- static/js/main.js
+++ static/js/main.js
@@ -1,41 +1,30 @@
-// CAROUSEL
 function showSection(sectionId) {
   const carousel = document.querySelector("#carouselExample");
-  const items = carousel.querySelectorAll(".carousel-item");
+  const activeItem = carousel.querySelector(".carousel-item.active");
+  const newItem = carousel.querySelector(`#${sectionId}`);
 
-  items.forEach((item) => {
-    item.classList.remove("active");
-    if (item.id === sectionId) {
-      item.classList.add("active");
-    }
-  });
+  if (activeItem !== newItem) {
+    activeItem.classList.remove("active");
+    newItem.classList.add("active");
+  }
 }
 
-// NAV
-fetch('../templates/partials/nav.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('nav').innerHTML = data;
-  })
-  .catch(error => console.error('Erro ao carregar o nav:', error));
 
-// MAIN
-fetch('../templates/content/main.html')
-  .then(response => response.text())
-  .then(data => {
-    console.log('Main loaded');
-    document.getElementById('main').innerHTML = data;
-  })
-  .catch(error => console.error('Erro ao carregar o main:', error));
+function loadComponent(componentId, filePath) {
+  fetch(filePath)
+    .then(response => response.text())
+    .then(data => {
+      console.log(`${componentId} loaded`);
+      document.getElementById(componentId).innerHTML = data;
+    })
+    .catch(error => console.error(`Erro ao carregar ${componentId}:`, error));
+}
 
-// FOOTER
-fetch('../templates/partials/footer.html')
-  .then(response => response.text())
-  .then(data => {
-    console.log('Footer loaded');
-    document.getElementById('footer').innerHTML = data;
-  })
-  .catch(error => console.error('Erro ao carregar o footer:', error));
+// Carregar os componentes
+loadComponent('nav', '../templates/partials/nav.html');
+loadComponent('main', '../templates/content/main.html');
+loadComponent('footer', '../templates/partials/footer.html');
+
 
 
 // LANG
@@ -43,18 +32,23 @@ function setLanguage(language) {
   const elementsToTranslate = document.querySelectorAll("[data-translate]");
   elementsToTranslate.forEach((element) => {
     const translationKey = element.getAttribute("data-translate");
-    element.innerHTML = translations[language][translationKey];
+    const translation = translations[language][translationKey] || translationKey;
+    element.innerHTML = translation;
   });
+
   const languageDropdownButton = document.getElementById("languageDropdown");
   languageDropdownButton.innerHTML =
     language === "en" ? "EN" : language === "br" ? "BR" : "JP";
+
   localStorage.setItem("selectedLanguage", language);
 }
 
+
+function initializeLanguage() {
+  const selectedLanguage = localStorage.getItem("selectedLanguage") || "en";
+  setLanguage(selectedLanguage);
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-  const selectedLanguage = localStorage.getItem("selectedLanguage");
-  
-  if (selectedLanguage) {
-    setLanguage(selectedLanguage);
-  }
+  initializeLanguage();
 });
