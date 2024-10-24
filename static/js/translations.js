2fca5db0859dc77c9ee8647c0917788ee2cc79fa
iSHIUBA <168146145+iamshiuba@users.noreply.github.com>
2024-10-24 10:46:20 -0300

    Update b4.0.2

--- static/js/translations.js
+++ static/js/translations.js
@@ -5,12 +5,12 @@ const translations = {
     about: "About",
     greeting: "Hello, world!",
     mainMessage:
-      "The website is still under construction, but you can check out some of my videos, my about page or my GitHub.",
+      "The website is still under construction, but you can check out some of my videos, my about page, <br> or my social networks that are present in the website's footer.",
     highlight: "Highlights",
     footer: "All rights reserved.",
     videosTitle: "Videos",
     videosDescription:
-      "This is the videos section, here are some videos to watch:",
+      "This is the video section, here are some videos to watch:",
     aboutTitle: "About",
     aboutMessage:
       "I made this website for fun. Hope you enjoy coz I'm about to make it better and better!<br/> Thank you for visiting!",
@@ -24,8 +24,8 @@ const translations = {
     about: "Sobre",
     greeting: "Olá, mundo!",
     mainMessage:
-      "O site ainda está em construção, mas você pode conferir alguns dos meus vídeos, a minha página sobre ou meu GitHub.",
-    highlight: "Destaque",
+      "O site ainda está em construção, mas você pode conferir alguns dos meus vídeos, minha página sobre <br> ou minhas redes sociais que estão presentes no rodapé do site.",
+    highlight: "Destaques",
     footer: "Todos os direitos reservados.",
     videosTitle: "Vídeos",
     videosDescription:
@@ -33,27 +33,27 @@ const translations = {
     aboutTitle: "Sobre",
     aboutMessage:
       "Eu fiz este site por diversão. Espero que você goste, pois estou prestes a melhorá-lo cada vez mais!<br/> Obrigado pela visita!",
-    Homepage:  "Página Inicial",
+    Homepage: "Página Inicial",
     Videos: "Vídeos",
     About: "Sobre",
   },
   jp: {
     ishiuba: "アイシウバ",
     videos: "ビデオ",
-    about: "私について",
+    about: "概要",
     greeting: "こんにちは、世界！",
     mainMessage:
-      "ウェブサイトはまだ建設中ですが、 ビデオ、自己紹介 セクション、またはGitHubをご覧いただけます。",
-    highlight: "見どころ",
+      "ウェブサイトはまだ建設中ですが、いくつかのビデオ、概要ページ、<br>またはフッターにあるソーシャルネットワークをチェックできます。",
+    highlight: "ハイライト",
     footer: "全著作権所有。",
     videosTitle: "ビデオ",
     videosDescription:
-      "これはビデオセクションです。 視聴すべきビデオをいくつか紹介します:",
-    aboutTitle: "私について",
+      "これはビデオセクションです。ここにいくつかの視聴するビデオがあります:",
+    aboutTitle: "概要",
     aboutMessage:
-      "私はこのウェブサイトを楽しむために作りました。 あなたが楽しんでくれることを願っています！ これからますます良くしていきます！<br/> 訪問していただきありがとうございます！",
+      "私はこのウェブサイトを楽しみのために作りました。気に入ってもらえると嬉しいです。<br/>どんどん改善していくつもりです！訪問してくれてありがとうございます！",
     Homepage: "ホームページ",
     Videos: "ビデオ",
-    About: "私について",
+    About: "概要",
   },
 };
